e86152f702be57dfa859f1031b35a016dfdac0d6
Sara Muntean <munteansr@gmail.com>
2018-07-22 17:29:57 -0400

    Resolved test and reworked old ones to use objects instead of booleans

--- week4.js
+++ week4.js
@@ -77,7 +77,7 @@ console.assert(capitalize("hello world") === "Hello World", "Capitalize a string
 var creditValidate = function(cardNumber) {
   stringNum = cardNumber.toString();
 
-  if (stringNum.substring(0,2) === '37') {
+  if (stringNum.substring(0,2) === '37' || stringNum.substring(0,2) === '34') {
     return (stringNum.length === 15);
   }
 
@@ -87,11 +87,13 @@ var creditValidate = function(cardNumber) {
 
 //Assertions
 
-console.assert(creditValidate(123456789012345) === false, 'String must be 16 digits unless 37 or 34');
-console.assert(creditValidate(1234567890123456) === true, 'String must be 16 digits unless 37 or 34');
+console.assert(creditValidate(123456789012345) === {valid: false}, 'String must be 16 digits unless 37 or 34');
+console.assert(creditValidate(1234567890123456) === {valid: true}, 'String must be 16 digits unless 37 or 34');
 
-console.assert(creditValidate(370000000000000) === true, "String must be 15 digits if starts with 37")
-console.assert(creditValidate(3700000000000000) === false, "String must not be 16 digits if starts with 37");
+console.assert(creditValidate(370000000000000) === {valid: true}, "String must be 15 digits if starts with 37")
+console.assert(creditValidate(3700000000000000) === {valid: false}, "String must not be 16 digits if starts with 37");
 
-console.assert(creditValidate(340000000000000) === true, "String must be 15 digits if starts with 34")
-console.assert(creditValidate(3400000000000000) === false, "String must not be 16 digits if starts with 34");
+console.assert(creditValidate(340000000000000) === {valid: true}, "String must be 15 digits if starts with 34")
+console.assert(creditValidate(3400000000000000) === {valid: false}, "String must not be 16 digits if starts with 34");
+
+
